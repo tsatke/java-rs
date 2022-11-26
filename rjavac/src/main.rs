faadb6ff7d57f601d5a8adab56ef9c4f7b8616f4
tsatke <tim.satke@gmail.com>
2022-11-26 17:27:33 +0100

    introduce clap CLI

--- rjavac/src/main.rs
+++ rjavac/src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    verbose: bool,
+}
+
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
+    if args.verbose {
+        println!("Running in verbose mode");
+    }
 }
